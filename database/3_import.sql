9c6e96cdb72db0660e2aa364cea402a5b7f1b146
MAREK <marek.armulik@gmail.com>
2024-04-10 14:19:56 +0300

    initial commit

--- database/3_import.sql
+++ database/3_import.sql
@@ -1,6 +1,6 @@
-INSERT INTO repair.brand (id, name) VALUES (8, 'Audi');
-INSERT INTO repair.brand (id, name) VALUES (9, 'BMW');
-INSERT INTO repair.brand (id, name) VALUES (10, 'Mercedes-Benz');
-INSERT INTO repair.brand (id, name) VALUES (11, 'Volkswagen');
-INSERT INTO repair.brand (id, name) VALUES (12, 'Volvo');
-INSERT INTO repair.brand (id, name) VALUES (13, 'Kondimootor');
+INSERT INTO repair.brand (id, name) VALUES (1, 'Audi');
+INSERT INTO repair.brand (id, name) VALUES (2, 'BMW');
+INSERT INTO repair.brand (id, name) VALUES (3, 'Mercedes-Benz');
+INSERT INTO repair.brand (id, name) VALUES (4, 'Volkswagen');
+INSERT INTO repair.brand (id, name) VALUES (5, 'Volvo');
+INSERT INTO repair.brand (id, name) VALUES (6, 'Kondimootor');
