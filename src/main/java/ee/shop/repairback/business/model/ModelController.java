2047529ffe6183791b92c442e6beca4a5e492fd2
Alar <alar.urke@gmail.com>
2024-04-11 12:00:03 +0300

    ModelController

--- src/main/java/ee/shop/repairback/business/model/ModelController.java
+++ src/main/java/ee/shop/repairback/business/model/ModelController.java
@@ -1,4 +1,21 @@
 package ee.shop.repairback.business.model;
 
+import lombok.AllArgsConstructor;
+import org.aspectj.asm.AsmManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class ModelController {
+
+    private ModelService modelService;
+
+    @GetMapping("/repair/model")
+
+   public List<ModelInfo> getModel(){
+
+   }
 }
