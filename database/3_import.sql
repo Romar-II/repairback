596345b7d55ce400464171c99e3d526ec8bd7af0
MAREK <marek.armulik@gmail.com>
2024-04-12 09:25:13 +0300

    Databases for main category creation

--- database/3_import.sql
+++ database/3_import.sql
@@ -9,3 +9,11 @@ INSERT INTO repair.model (id, brand_id, name) VALUES (1, 1, 'A5');
 INSERT INTO repair.model (id, brand_id, name) VALUES (2, 1, 'A6');
 INSERT INTO repair.model (id, brand_id, name) VALUES (3, 2, 'M4');
 INSERT INTO repair.model (id, brand_id, name) VALUES (4, 2, 'M5');
+
+
+INSERT INTO repair.repair_main_category (id, name) VALUES (1, 'Mootor');
+INSERT INTO repair.repair_main_category (id, name) VALUES (2, 'Veermik');
+INSERT INTO repair.repair_main_category (id, name) VALUES (3, 'Elekter');
+INSERT INTO repair.repair_main_category (id, name) VALUES (4, 'Kere');
+INSERT INTO repair.repair_main_category (id, name) VALUES (5, 'Rehvid');
+INSERT INTO repair.repair_main_category (id, name) VALUES (6, 'Klaasid');
