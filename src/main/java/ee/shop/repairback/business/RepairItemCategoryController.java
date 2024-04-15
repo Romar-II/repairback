702e5c654fad8e63f01116f3ceb507e3c3ca059e
Stiven <stiven161@gmail.com>
2024-04-15 10:51:26 +0300

    RepairItemCategory + database

---
+++ src/main/java/ee/shop/repairback/business/RepairItemCategoryController.java
@@ -0,0 +1,13 @@
+package ee.shop.repairback.business;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+
+public class RepairItemCategoryController {
+
+
+
+}
---
+++ src/main/java/ee/shop/repairback/business/RepairItemCategoryService.java
@@ -0,0 +1,10 @@
+package ee.shop.repairback.business;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RepairItemCategoryService {
+
+}
