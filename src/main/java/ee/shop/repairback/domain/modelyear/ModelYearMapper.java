1e14fac60bec4c817b53d9966c3892b961694020
Alar <alar.urke@gmail.com>
2024-04-16 15:01:39 +0300

    ModelController

--- src/main/java/ee/shop/repairback/domain/modelyear/ModelYearMapper.java
+++ src/main/java/ee/shop/repairback/domain/modelyear/ModelYearMapper.java
@@ -1,11 +1,20 @@
 package ee.shop.repairback.domain.modelyear;
 
+import ee.shop.repairback.business.model.dto.ModelInfo;
 import ee.shop.repairback.business.year.dto.ModelYearInfo;
+import ee.shop.repairback.domain.model.Model;
 import org.mapstruct.*;
+
+import java.time.Year;
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ModelYearMapper {
 
+    @Mapping(source = "number", target ="yearNumber")
+    @Mapping(source = "id", target = "yearId")
     ModelYearInfo toDto(ModelYear modelYear);
+
+    //List<ModelYearInfo> toModelInfos(List<Model> models);
 
 }
