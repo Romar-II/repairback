4bdd1cb912a9373396962e86042b023e862639ab
MAREK <marek.armulik@gmail.com>
2024-04-17 15:28:06 +0300

    Databases for main category creation

--- src/main/java/ee/shop/repairback/business/shopproducts/ShopProductsController.java
+++ src/main/java/ee/shop/repairback/business/shopproducts/ShopProductsController.java
@@ -4,7 +4,6 @@ package ee.shop.repairback.business.shopproducts;
 import ee.shop.repairback.business.shopproducts.dto.ProductInfo;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
