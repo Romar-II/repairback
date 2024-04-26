6946adcd5483c8f2cf38215988717e0860b0585c
Stiven <stiven161@gmail.com>
2024-04-26 11:50:04 +0300

    Login

--- src/main/java/ee/shop/repairback/business/login/LoginController.java
+++ src/main/java/ee/shop/repairback/business/login/LoginController.java
@@ -4,6 +4,7 @@ package ee.shop.repairback.business.login;
 import ee.shop.repairback.business.login.dto.UserInfo;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,13 @@ public class LoginController {
    public UserInfo login(@RequestParam String username, @RequestParam String password) {
         return loginService.login(username, password);
     }
+
+    @PostMapping("/registration")
+        public void addNewUser (@RequestParam String username, @RequestParam String password) {
+        loginService.addNewUserRegistration(username,password);
+
+    }
+
+
 
 }
--- src/main/java/ee/shop/repairback/business/login/LoginService.java
+++ src/main/java/ee/shop/repairback/business/login/LoginService.java
@@ -2,6 +2,8 @@ package ee.shop.repairback.business.login;
 
 import ee.shop.repairback.business.login.dto.UserInfo;
 import ee.shop.repairback.domain.infrastrcutre.ValidationService;
+import ee.shop.repairback.domain.role.Role;
+import ee.shop.repairback.domain.role.RoleRepository;
 import ee.shop.repairback.domain.user.User;
 import ee.shop.repairback.domain.user.UserMapper;
 import ee.shop.repairback.domain.user.UserRepository;
@@ -17,11 +19,23 @@ public class LoginService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final RoleRepository roleRepository;
 
     public UserInfo login(String username, String password) {
         Optional<User> optionalUser = userRepository.findUserBy(username, password, Status.ACTIVE);
         User user = ValidationService.getValidExistingUser(optionalUser);
         return userMapper.toUserInfo(user);
+    }
+
+    public void addNewUserRegistration(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setStatus(Status.ACTIVE);
+        Role role = roleRepository.getReferenceById(2);
+        role.setId(2);
+        userRepository.save(user);
+
     }
 
 }
