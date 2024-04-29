bf593fe9019e7fa6e769f9e3772221303d45ecae
MAREK <marek.armulik@gmail.com>
2024-04-29 11:34:22 +0300

    Databases for main category creation

--- src/main/java/ee/shop/repairback/business/cart/CartController.java
+++ src/main/java/ee/shop/repairback/business/cart/CartController.java
@@ -26,9 +26,6 @@ public class CartController {
 
     @GetMapping("/cart/update/{userId}")
     public Integer  updateCartQty(@PathVariable Integer userId){
-        if (userId == null) {
-            return 0;
-        }
             return cartService.updateCartQty(userId);
 
     }
@@ -39,11 +36,11 @@ public class CartController {
         return cartService.getCartItems(userId);
     }
 
-//    @GetMapping("/cart/history/{userId}")
-//    public List<OrderInfo> getOrderHistoryItems(@PathVariable Integer userId){
-//
-//        return cartService.getOrderHistoryItems(userId);
-//    }
+    @GetMapping("/cart/history/{userId}")
+    public List<OrderInfo> getOrderHistoryItems(@PathVariable Integer userId){
+
+        return cartService.getOrderHistoryItems(userId);
+    }
 
     @PutMapping("/basket/empty")
     public void deletePendingOrder(@RequestParam Integer userId) {
